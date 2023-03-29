af0d4ac58f2d450b6691d5c2f96154c71f807c3f
Seyoung Park <53702978+seyoung8239@users.noreply.github.com>
2023-03-29 22:23:14 +0900

    [level 2] Title: 후보키, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42890. 후보키/후보키.js
+++ 프로그래머스/lv2/42890. 후보키/후보키.js
@@ -1,47 +1,28 @@
-const comb = [];
-let isSuperSet = [];
-let ans = 0;
-
-function bt(n, i, m, cnt, acc) {
-    if(n === i && m === cnt) {
-        comb.push(acc);
-        return;
-    }
-    if(n === i) return;
-    bt(n, i+1, m, cnt+1, acc.concat([1]))
-    bt(n, i+1, m, cnt, acc.concat([0]))
-}
-
-function isCandidate(relation, c) {
-    const set = new Set();
-    relation.forEach(r => {
-        const cur = r.filter( (_, i) => c[i]).join('_');
-        set.add(cur);    
-    })
-    return set.size === relation.length;
-}
-
-function checkSuperSet(ii) {
-    const cur = comb[ii];
-    for(let i=ii; i<comb.length; i++) {
-        let cur_ = cur.map((el, j) => el && !comb[i][j])
-        const check = cur_.every(el => !el);
-        if(check) isSuperSet[i] = true;
-    }
-}
-
 function solution(relation) {
-    const [n,m] = [relation.length, relation[0].length];
-    for(let i=1; i<=m; i++) bt(m, 0, i, 0, []);
-    isSuperSet = new Array(comb.length).fill(false);
-
-    for(let i=0; i<comb.length; i++) {
-        if(isSuperSet[i]) continue;
-        if(isCandidate(relation, comb[i])) {
-            ans++;
-            checkSuperSet(i);
+    const [row, col] = [relation.length, relation[0].length];
+    const ansList = [];
+    const minimalList = [];
+    
+    for(let i=1; i<(1<<col); i++) {
+        const set = new Set();
+        for(let j=0; j<row; j++) {
+            let uniq = "";
+            for(let k=0; k<col; k++) {
+                if(i & (1<<k))
+                    uniq += relation[j][k];
+            }
+            set.add(uniq);
         }
+        if(set.size === row && isMinimal(i, ansList)) ;
     }
     
-    return ans;
+    function isMinimal(cur, list) {
+        for(let i=0; i<list.length; i++) {
+            if((list[i] & cur) === list[i]) return false;
+        }
+        list.push(cur);
+        return true;
+    }
+    
+    return ansList.length;
 }
